fe13233cffc02041053f446c6d043e122e833243
Briansaint2019 <briansaint88@outlook.com>
2018-07-02 21:20:05 -0400

    first commit

---
+++ quiz.js
@@ -0,0 +1,39 @@
+
+
+// Q1 a.
+
+var i = 365 * 2 ; 
+//console.log(i)
+//q1 b.
+
+var str6 = "this is not " + "what i signed " + "up for!!!! " + "this is unnecessary " +"help me please!"; 
+//console.log(str6)
+
+//q1 d
+
+    var money = ['4', '3', '2', '1', 'lets go', 'Cowboy Bebop', '3 > 1', '3 == 3', '2 != 1'], ['hi' , '5', 'tik tok'];
+
+
+    // Q2
+
+ document.getElementByTagName(h2);
+
+document.getElementByClass(helloWorld);
+
+ document.getElementById(keys);
+
+ document.getElementByType(type)
+
+document.getElementByType(button)
+
+
+
+// Q3
+var newbutton = document.querySelector('.help')
+newbutton.addEventListener('click', function(e){
+
+
+
+
+
+})
